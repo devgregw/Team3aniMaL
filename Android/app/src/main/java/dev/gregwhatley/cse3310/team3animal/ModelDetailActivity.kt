040995ae097fe34dd05c69533c8b07a2e9af4d22
Greg Whatley <me@gregwhatley.dev>
2020-04-28 11:12:49 -0500

    Model detail activity

---
+++ Android/app/src/main/java/dev/gregwhatley/cse3310/team3animal/ModelDetailActivity.kt
@@ -0,0 +1,61 @@
+package dev.gregwhatley.cse3310.team3animal
+
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.gms.tasks.Tasks
+import kotlinx.android.synthetic.main.activity_model_detail.*
+import java.io.ByteArrayOutputStream
+
+class ModelDetailActivity : AppCompatActivity() {
+    companion object {
+        const val MENU_TOP_ID = 1
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menu?.add(Menu.NONE, MENU_TOP_ID, 0, "Top")?.setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_ALWAYS)
+            ?.setIcon(R.drawable.ic_action_top)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home)
+            finish()
+        if (item.itemId == MENU_TOP_ID) {
+            vertical_scroll_view.smoothScrollTo(0, 0)
+            horizontal_scroll_view.smoothScrollTo(0, 0)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_model_detail)
+        supportActionBar?.apply {
+            setDisplayShowHomeEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_action_back)
+            setDisplayHomeAsUpEnabled(true)
+        }
+        title = "ML Model Information"
+        model_detail.text = "Loading..."
+        Tasks.call {
+            val input = resources.openRawResource(R.raw.model_data)
+            val buffer = ByteArray(input.available())
+            input.read(buffer)
+            val output = ByteArrayOutputStream()
+            output.write(buffer)
+            output.close()
+            input.close()
+            val string = output.toString("UTF-8")
+            Handler(mainLooper).post {
+                model_detail.typeface = Typeface.MONOSPACE
+                model_detail.setTextIsSelectable(true)
+                model_detail.textSize = 10f
+                model_detail.text = string
+            }
+        }
+    }
+}
